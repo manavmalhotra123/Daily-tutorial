0426bfcfcf3572a87e4eec299e5a86516dc49758
manav <mmalhotra_be21@thapar.edu>
2022-11-09 14:32:05 +0530

    taking number as an input from user

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<map>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    int n; // initialization of variable
+    cout<<"Enter the numeber n "<<endl;
+    cin >> n;
+    cout<<"n = "<<n<<endl;   
+    return 0;
+}
