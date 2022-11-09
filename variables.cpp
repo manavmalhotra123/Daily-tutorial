c54f29788283440f4787f7b9e7052cf51ec383e3
Manav Malhotra <110531978+manavmalhotra123@users.noreply.github.com>
2022-11-09 14:40:49 +0530

    Update variables.cpp
    basic data-types of cpp

--- variables.cpp
+++ variables.cpp
@@ -3,5 +3,20 @@ using namespace std;
 int main(){
   int a; // int type variable
   float b; // float type variable
-  char c;
+  char c; // character type variable;
+  
+  a = 9;
+  b = 9.0;
+  c = 'a';
+  
+  // for printing the data-types stored data
+  cout<<"a = "<<a<<endl;
+  cout<<"b = "<<b<<endl;
+  cout<<"c = "<<c<<endl;
+  
+  
+  // for checking the size of the datatypes
+  cout<<sizeof(a)<<endl;
+  cout<<sizeof(b)<<endl;
+  cout<<sizeof(c)<<endl;
   
