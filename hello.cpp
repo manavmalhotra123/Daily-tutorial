031a972cdf7353f4a30528ee999f2ad8fc1d3157
manav <mmalhotra_be21@thapar.edu>
2022-11-09 14:27:49 +0530

    initial

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<map>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
